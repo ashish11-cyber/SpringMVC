ce28d4d245306e974ebf2825eff9c46978d1e92a
Ashish Singh Chauhan <ashishsinghc128@gmail.com>
2021-04-01 02:20:29 +0530

    Initial commit

---
+++ src/main/java/org/springframework/streotype/HelloController.java
@@ -0,0 +1,15 @@
+package org.springframework.streotype;
+import org.springframework.streotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+@RequestMapping("")
+@Controller
+public class HelloController {
+@RequestMapping(method = RequestMethod.GET)
+ public String printHello(ModelMap model)
+{
+    model.addAttribute(atrributeName="message", attributeValue ="Hello Spring MVC Framework!");
+    return "hello";
+}
+}
